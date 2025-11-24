4c25a313196341323f312de4acb908478e495bb6
JackSSads <contatojacksonsouza@hotmail.com>
2025-11-24 14:20:53 -0300

    Separando componentes

--- src/pages/manageUser/index.jsx
+++ src/pages/manageUser/index.jsx
@@ -4,7 +4,11 @@ import { ManagerUser, Settings, Categories, Navbar } from "../../components";
 
 export const ManageUser = () => {
 
-    const [showComponent, setShowComponent] = useState(1);
+    const [showComponent, setShowComponent] = useState(0);
+
+    const Viewer = () => {
+        return showComponent === 0 ? (<ManagerUser key={1} />) : showComponent === 1 ? (<Categories key={1} />) : (<Settings key={2} />)
+    };
 
     return (
         <>
@@ -12,25 +16,17 @@ export const ManageUser = () => {
             <div className="p-2 flex flex-col w-full xl:w-[1000px] h-[90dvh] justify-start items-center">
 
                 <div className="w-full flex justify-between items-center gap-2">
-                    <button
-                        className={`${showComponent === 1 && "bg-[#EB8F00] text-white border-[#EB8F00]"} border-2 border-[#1C1D26] transition-all delay-75 w-1/3 text-center p-2 rounded-md font-semibold`}
-                        onClick={() => setShowComponent(1)}>
-                        Usuários
-                    </button>
-                    <button
-                        className={`${showComponent === 2 && "bg-[#EB8F00] text-white border-[#EB8F00]"} border-2 border-[#1C1D26] transition-all delay-75 w-1/3 text-center p-2 rounded-md font-semibold`}
-                        onClick={() => setShowComponent(2)}>
-                        Categorias
-                    </button>
-                    <button
-                        className={`${showComponent === 3 && "bg-[#EB8F00] text-white border-[#EB8F00]"} border-2 border-[#1C1D26] transition-all delay-75 w-1/3 text-center p-2 rounded-md font-semibold`}
-                        onClick={() => setShowComponent(3)}>
-                        Configurações
-                    </button>
+                    {["Usuários", "Categorias", "Configurações"].map((header, index) => (
+                        <button
+                            key={index}
+                            className={`${showComponent === index && "bg-[#EB8F00] text-white border-[#EB8F00]"} border-2 border-[#1C1D26] transition-all delay-75 w-1/3 text-center p-2 rounded-md font-semibold`}
+                            onClick={() => setShowComponent(index)}
+                        >
+                            {header}
+                        </button>
+                    ))}
                 </div>
-                <ManagerUser showComponent={showComponent} />
-                <Categories showComponent={showComponent} />
-                <Settings showComponent={showComponent} />
+                <Viewer />
             </div>
         </>
     );
