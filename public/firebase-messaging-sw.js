88a0efdcb080d7680c8e07b0b91bf759e921c821
JackSSads <contatojacksonsouza@hotmail.com>
2025-11-24 14:32:25 -0300

    Removendo ícone temporariamente

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -28,7 +28,6 @@ messaging.onBackgroundMessage((payload) => {
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: "./logo.png",
     data: { url: link },
   };
 
